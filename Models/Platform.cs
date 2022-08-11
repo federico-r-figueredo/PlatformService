7937e08cd34270eaa73ebbfeab7190afc88f880f
Federico R. Figueredo <federico.r.figueredo@protonmail.com>
2022-08-11 11:11:50 -0300

    Adding Entities

--- Models/Platform.cs
+++ Models/Platform.cs
@@ -1,9 +1,9 @@
 
 using System.ComponentModel.DataAnnotations;
 
-namespace PlatformService;
+namespace PlatformService.Models;
 
-public class Platform
+public class Platform : IEntity
 {
     [Key]
     [Required]
